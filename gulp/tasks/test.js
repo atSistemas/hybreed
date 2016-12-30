9f7fbd05acb3f3c8da2a23fd06f4cb1ac970e195
Manuel Alfonso Terol <malfonso@atsistemas.com>
2016-12-30 14:36:14 +0100

    code coverage

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -1,35 +1,95 @@
 var gulp = require('gulp'),
     mocha = require('gulp-mocha'),
-    babel = require('babel-register');
+    babel = require('babel-register'),
+    istanbul = require('gulp-istanbul'),
+    isparta = require('isparta'),
+
+    exec = require('gulp-exec'),
+    del = require('del'),
+    coverageEnforcer = require('gulp-istanbul-enforcer');
 
 
+var paths = {
+  scripts:  ['src/**/*.js'],
+  tests:    ['test/**/*.spec.js'],
+  coverage: 'coverage/'
+};
+
+// Import dom util to avoid errors about window, navigator... not found
+var dom = require('../../test/utils/dom');
+
+
+// INTERNAL TASKS
+//
+gulp.task('clean-coverage', function(cb) {
+    return del(['coverage'], cb);
+});
+
+
+// TASKS
+//
 gulp.task('test', function() {
-    return gulp.src(['test/**/*.spec.js'])
+    return gulp.src(paths.tests, {read:false})
         .pipe(mocha({
             compilers:babel,
             require:['jsdom-global/register']
         }));
 });
+
 
 gulp.task('testFromSource', function() {
     return gulp.src(['src/**/*.spec.js'])
         .pipe(mocha({
             compilers:babel,
             require:['jsdom-global/register']
-        }));
+        }))
+        .pipe(istanbul)
+        .pipe(istanbul.writeReports());
 });
 
 gulp.task('tdd', function() {
-    return gulp.watch(['src/**/*.js','test/**/*.js'], ['test']);
+    return gulp.watch([paths.scripts,paths.tests], ['test']);
 });
 
 gulp.task('tdd-single', function() {
-    return gulp.watch('test/**/*.spec.js')
+    return gulp.watch(paths.tests)
         .on('change', function(file) {
             gulp.src(file.path)
                 .pipe(mocha({
                     compilers: babel,
                     require:['jsdom-global/register']
                 }))
+        });
+});
+
+
+
+gulp.task('test-coverage', ['clean-coverage'], function(cb) {
+    var coverageDir = paths.coverage;
+    gulp.src(paths.scripts)
+        .pipe(istanbul({ // Covering files
+            instrumenter: isparta.Instrumenter,
+            includeUntested: true
+        }))
+        .pipe(istanbul.hookRequire()) // Force `require` to return covered files
+        .on('finish', function() {
+            gulp.src(paths.tests, {read: false})
+                .pipe(mocha({reporter: 'spec'}))
+                .pipe(istanbul.writeReports({
+                    dir: coverageDir,
+                    reportOpts: {dir: coverageDir},
+                    reporters: ['text', 'text-summary', 'json', 'html']
+                }))
+                .pipe(coverageEnforcer({
+                    thresholds: {
+                        statements: 80,
+                        branches: 50,
+                        lines: 80,
+                        functions: 50
+                    },
+                    coverageDirectory: coverageDir,
+                    rootDirectory : ''
+                }))
+                .on('end', cb);
         });
 });
---
+++ test/js/login.spec.js
@@ -0,0 +1,45 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+chai.should();
+var expect = chai.expect;
+chai.use(sinonChai);
+
+import * as Libs from '~/src/vendor/libs'
+// import Login from '~/src/modules/login/index';
+//
+//
+// describe('LOGIN MODULE', () => {
+//   var testContext = {};
+//   var logInfo;
+//
+//   beforeEach(() => {
+//       logInfo = null;
+//       testContext.mainModule = Login;
+//       testContext.broker = Libs.Broker;
+//   });
+//
+//   afterEach(() => {
+//     if (logInfo != null){
+//       console.log("||--------------||");
+//       console.log(logInfo);
+//       console.log("||--------------||");
+//     }
+//   })
+//
+//   it('should return a start function', () => {
+//     expect(testContext.mainModule.start).to.exist;
+//     testContext.mainModule.start.should.be.a('function');
+//   });
+//
+//   it('should return a getUserLogged function', () => {
+//     expect(testContext.mainModule.getUserLogged).to.exist;
+//     testContext.mainModule.getUserLogged.should.be.a('function');
+//   });
+//
+//   it('should return a logout function', () => {
+//     expect(testContext.mainModule.logout).to.exist;
+//     testContext.mainModule.logout.should.be.a('function');
+//   });
+//
+// });
