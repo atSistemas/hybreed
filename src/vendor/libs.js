8579a36876ba7f89a41d5070f51892d00966e66e
Carlos Reyes Jiménez Barral <creyes@atsistemas.com>
2017-01-16 18:20:50 +0100

    New Markdown

--- src/vendor/libs.js
+++ src/vendor/libs.js
@@ -5,11 +5,9 @@ import Marionette from 'backbone.marionette';
 import Broker from 'backbone.radio';
 import Hybreed from '~/src/vendor/hybreed/hybreed';
 import Snap from 'snapjs';
+import ionRangeslider from 'ion-rangeslider';
 
 window.$ = window.jQuery = global.jQuery = $;
-
-
-require('ion-rangeslider');
 
 export {
     _,
@@ -17,5 +15,6 @@ export {
     Marionette,
     Broker,
     Hybreed,
-    Snap
+    Snap,
+    ionRangeslider
 };
